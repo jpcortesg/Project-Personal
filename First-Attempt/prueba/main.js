1faf7aea8e0fd269a712459ab7cd7b3bfe956ba8
jpcortesg <juan2001cortes.jc@gmail.com>
2020-09-23 03:04:05 -0500

    starting new project personal, about barbershop

---
+++ First-Attempt/prueba/main.js
@@ -0,0 +1,6 @@
+const menuToggle = () => {
+  const barthree = document.querySelector('.FB')
+  const MenuItems = document.querySelector('.menu .items')
+  barthree.classList.toggle("active");
+  MenuItems.classList.toggle("active");
+}
