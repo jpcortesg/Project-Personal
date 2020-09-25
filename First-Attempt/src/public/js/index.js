62358219c649bea55d4f01620e31864c4d2e548d
jpcortesg <juan2001cortes.jc@gmail.com>
2020-09-25 18:02:14 -0500

    starting migration to sass

--- First-Attempt/src/public/js/index.js
+++ First-Attempt/src/public/js/index.js
@@ -1,6 +1,6 @@
 const menuToggle = () => {
-  const three = document.querySelector('.FB')
-  const items = document.querySelector('.menu .items')
+  const three = document.querySelector('.menu_toggle_sub_FB')
+  const items = document.querySelector('.menu_items')
   three.classList.toggle('active')
   items.classList.toggle('active')
 }
